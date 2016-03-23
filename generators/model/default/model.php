ca9d4fcee8dec5143ff89d2c2d16e8e19a3777e0
gilles.grandguillaume <gilles.grandguillaume@segic.usach.cl>
2016-03-23 10:06:37 -0300

    Auto generación de lista con opciones segun los nombres de campos de la BDD

--- generators/model/default/model.php
+++ generators/model/default/model.php
@@ -2,7 +2,6 @@
 /**
  * This is the template for generating the model class of a specified table.
  */
-
 /* @var $this yii\web\View */
 /* @var $generator aayaresko\gii\generators\model\Generator */
 /* @var $tableName string full table name */
@@ -13,6 +12,14 @@
 /* @var $rules string[] list of validation rules */
 /* @var $relations array list of relations (name => relation declaration) */
 
+
+/**
+ * Si el modelo contiene un campo id y otro nombre, se generar el metodo getOptionList
+ */
+$generateGetOptionListMethod = false;
+$countFields = 0;
+
+
 echo "<?php\n";
 ?>
 
@@ -21,79 +28,103 @@ namespace <?= $generator->ns ?>;
 use Yii;
 
 /**
- * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
- *
+* This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+*
 <?php foreach ($tableSchema->columns as $column): ?>
- * @property <?= "{$column->phpType} \${$column->name}\n" ?>
+    * @property <?= "{$column->phpType} \${$column->name}\n" ?>
+    <?php if ($column->name == 'id' || $column->name == 'nombre') {
+        $countFields++;
+    } ?>
 <?php endforeach; ?>
+<?php if ($countFields == 2) {
+    $generateGetOptionListMethod = true;
+} ?>
 <?php if (!empty($relations)): ?>
- *
-<?php foreach ($relations as $name => $relation): ?>
- * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
-<?php endforeach; ?>
+    *
+    <?php foreach ($relations as $name => $relation): ?>
+        * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+    <?php endforeach; ?>
 <?php endif; ?>
- */
+*/
 class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?>
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return '<?= $generator->generateTableName($tableName) ?>';
-    }
+/**
+* @inheritdoc
+*/
+public static function tableName()
+{
+return '<?= $generator->generateTableName($tableName) ?>';
+}
 <?php if ($generator->db !== 'db'): ?>
 
     /**
-     * @return \yii\db\Connection the database connection used by this AR class.
-     */
+    * @return \yii\db\Connection the database connection used by this AR class.
+    */
     public static function getDb()
     {
-        return Yii::$app->get('<?= $generator->db ?>');
+    return Yii::$app->get('<?= $generator->db ?>');
     }
 <?php endif; ?>
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [<?= "\n            " . implode(",\n            ", $rules) . ",\n        " ?>];
-    }
+/**
+* @inheritdoc
+*/
+public function rules()
+{
+return [<?= "\n            " . implode(",\n            ", $rules) . ",\n        " ?>];
+}
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
+/**
+* @inheritdoc
+*/
+public function attributeLabels()
+{
+return [
 <?php foreach ($labels as $name => $label): ?>
-            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+    <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
 <?php endforeach; ?>
-        ];
-    }
+];
+}
 <?php foreach ($relations as $name => $relation): ?>
 
     /**
-     * @return \yii\db\ActiveQuery
-     */
+    * @return \yii\db\ActiveQuery
+    */
     public function get<?= $name ?>()
     {
-        <?= $relation[0] . "\n" ?>
+    <?= $relation[0] . "\n" ?>
     }
 <?php endforeach; ?>
 <?php if ($queryClassName): ?>
-<?php
+    <?php
     $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
     echo "\n";
-?>
+    ?>
     /**
-     * @inheritdoc
-     * @return <?= $queryClassFullName ?> the active query used by this AR class.
-     */
+    * @inheritdoc
+    * @return <?= $queryClassFullName ?> the active query used by this AR class.
+    */
     public static function find()
     {
-        return new <?= $queryClassFullName ?>(get_called_class());
+    return new <?= $queryClassFullName ?>(get_called_class());
+    }
+<?php endif; ?>
+<?php if ($generateGetOptionListMethod): ?>
+    /**
+    * Generates the data suitable for list-based HTML elements
+    **/
+    public function getOptionList()
+    {
+    return CHtml::listData($this->findAll(), 'id', 'nombre');
+    }
+<?php else: ?>
+    /**
+    * Generates the data suitable for list-based HTML elements
+    * stub version
+    **/
+    public function getOptionList()
+    {
+    return [];
     }
 <?php endif; ?>
 }
