a339bffef51f1b7f7a61c109024618e51df89622
gilles.grandguillaume <gilles.grandguillaume@segic.usach.cl>
2016-03-30 16:24:12 -0300

    fix syntaxis error

--- generators/crud/segic/views/_form.php
+++ generators/crud/segic/views/_form.php
@@ -14,8 +14,8 @@ if (empty($safeAttributes)) {
 }
 
 echo "<?php\n";
-echo $generator->tableSchemaContain('date')? 'use kartik\datecontrol\DateControl' : '';
-echo $generator->tableSchemaContain('datetime')? 'use kartik\datecontrol\DateControl' : '';
+echo $generator->tableSchemaContain('date')? 'use kartik\datecontrol\DateControl;' : '';
+echo $generator->tableSchemaContain('datetime')? 'use kartik\datecontrol\DateControl;' : '';
 
 //preg_grep("#^up_#", $generator->columnNames);
 
